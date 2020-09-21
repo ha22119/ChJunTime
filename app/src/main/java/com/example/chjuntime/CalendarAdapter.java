584e8c4ebe7dcbd66321cc7e1311e49e62a5bb31
ha22119 <55187236+ha22119@users.noreply.github.com>
2020-09-21 20:58:17 +0900

    20200921_야자
    Monthly xml 구현 완료 & 달력 나오게 하기 구현 완료

---
+++ app/src/main/java/com/example/chjuntime/CalendarAdapter.java
@@ -0,0 +1,38 @@
+package com.example.chjuntime;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+
+import java.util.ArrayList;
+
+public class CalendarAdapter extends BaseAdapter {
+    private ArrayList<String> dayList;
+    private Context mContext;
+    private int mResource;
+    private LayoutInflater mLiInflater;
+    GridView gridView;
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
--- app/src/main/java/com/example/chjuntime/MainActivity.java
+++ app/src/main/java/com/example/chjuntime/MainActivity.java
@@ -136,25 +136,29 @@ public class MainActivity extends AppCompatActivity {
     public void monthlyClick(View view) {
         Intent monthlyIntent = new Intent(getApplicationContext(),MonthlyActivity.class);
         startActivity(monthlyIntent);
-//        Toast.makeText(this, "아직 없는 액티비티", Toast.LENGTH_SHORT).show();
+        overridePendingTransition(0,0);
+        finish();
     }
 
     public void recordClick(View view) {
         Intent recordIntent = new Intent(getApplicationContext(),RecordActivity.class);
         startActivity(recordIntent);
         overridePendingTransition(0, 0);
+        finish();
     }
 
     public void groupClick(View view) {
         Intent groupIntent = new Intent(getApplicationContext(),GroupActivity.class);
         startActivity(groupIntent);
         overridePendingTransition(0, 0);
+        finish();
     }
 
     public void myClick(View view) {
         Intent myIntent = new Intent(getApplicationContext(),MyInfoActivity.class);
         startActivity(myIntent);
         overridePendingTransition(0, 0);
+        finish();
     }
 // 하단바 버튼 함수
 
